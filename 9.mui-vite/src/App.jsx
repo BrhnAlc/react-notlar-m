87b7c86a9b92828524372e21b7c91e6b5191a974
burhan <alacaburhan122016@gmail.com>
2024-03-16 21:25:28 +0300

    muı

--- 9.mui-vite/src/App.jsx
+++ 9.mui-vite/src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import { useState } from "react"
+
+
 
 
 
 function App() {
- 
+ const [count, setCount] = useState(0)
 
   return (
     <div>
-      MUI
+     <h1>MUI</h1>
+     {count}
+     <hr />
+     <button onClick={()=>setCount(count + 1)}>artır</button>
     </div>
   )
 }
